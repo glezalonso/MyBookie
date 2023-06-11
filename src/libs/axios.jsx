cac26ea66d3a49402a8ab91be95d97960e90eec9
glezalonso23 <glezalonso23@gmail.com>
2023-06-10 19:10:11 -0600

    Update axios.jsx

--- src/libs/axios.jsx
+++ src/libs/axios.jsx
@@ -3,6 +3,7 @@ import { useAuthStore } from "../store/auth"
 
 const authApi = axios.create({
     baseURL: "https://my-bookie-server-production.up.railway.app/",
+    withCredentials: true
 })
 
 authApi.interceptors.request.use(config => {
