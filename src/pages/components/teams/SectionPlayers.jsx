9a734826fcfab2f89165f1cf02bee078aa83099b
alonso <glezalonso23@gmail.com>
2023-06-16 16:04:34 -0600

    filter lineup

--- src/pages/components/teams/SectionPlayers.jsx
+++ src/pages/components/teams/SectionPlayers.jsx
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from 'react'
 import { getPlayers } from '../../../services/players'
 import { addPlayer } from '../../../services/teams'
 import toast from 'react-hot-toast'
-import { Button, Table, Alert } from 'react-bootstrap'
+import { Button, Table, Alert, FormControl } from 'react-bootstrap'
 
 const SectionPlayers = ({ team, setLoading }) => {
   const [players, setPlayers] = useState([])
+  const [dataFilter, setDataFilter] = useState('')
 
   useEffect(() => {
     getPlayers()
@@ -21,12 +22,25 @@ const SectionPlayers = ({ team, setLoading }) => {
       .catch(() => toast.error('failed to add player'))
       .finally(() => setLoading(false))
   }
+  const handleOnChange = (event) => {
+    setDataFilter(event.target.value)
+  }
 
   const playerBySport = players?.filter(player => player?.sport?._id === team?.sport?._id)
+
+  const filter = playerBySport.filter(player => {
+    if (dataFilter) {
+      return player?.fullName?.toLowerCase().includes(dataFilter.toLowerCase())
+    } else {
+      return player
+    }
+  }
+  )
 
   return (
         <>
             <h1 className="h4 static">All players</h1>
+            <FormControl placeholder='Search Player...' id='player' name='player' value={dataFilter} onChange={(event) => handleOnChange(event)} />
             <Table responsive variant="dark" hover striped>
                 <thead>
                     <tr>
@@ -35,8 +49,8 @@ const SectionPlayers = ({ team, setLoading }) => {
                     </tr>
                 </thead>
                 <tbody>
-            {(playerBySport.length > 0)
-              ? playerBySport?.map(player => (
+            {(filter.length > 0)
+              ? filter?.map(player => (
             <tr key={player?._id}><td>{player?.fullName}</td><td> <Button variant="warning" onClick={() => handleAdd(team?._id, player?._id, player?.fullName)}>Add</Button></td></tr>
               ))
               : <tr><td><Alert variant="info">There is no information to show!</Alert></td></tr>}
